ffc74051ba5744501838d9aa4a6b41c185648852
nikolas4175-godaddy <nikolas4175@godaddy.com>
2023-09-22 20:27:01 -0700

    update sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,18 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'SV Plugin x WooCommerce Blocks Integration Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/nikolas4175-godaddy/eng-ref-wc-blocks-integration',
 			},
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', link: '/guides/example/' },
+				// 	],
+				// },
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
